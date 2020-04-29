8bb0e5861bf06498544254e42ed480d996cbf15b
IrinaDogadina <62995764+IrinaDogadina@users.noreply.github.com>
2020-04-29 19:19:46 +0300

    Delete deleteANDsearch_lab3.hpp

--- deleteANDsearch_lab3.hpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <fstream>
-
-using namespace std;
-
-
-void Delete(Mag * Prod, int temp, string NameFile);
-void Search(Mag * Prod, int temp, int MenuRes, string parametr);
